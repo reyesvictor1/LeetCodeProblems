790430dbb0fa0156423d6851111d0dce006bd3f5
Victor Reyes <100883441+reyesvictor1@users.noreply.github.com>
2024-10-02 00:19:20 -0600

    Problem #735: C++ solution added.

--- 00735-asteroid-collision/main.cpp
+++ 00735-asteroid-collision/main.cpp
@@ -8,64 +8,70 @@ public:
     std::vector<int> asteroidCollision(std::vector<int>& asteroids) {
         std::stack<int> collisions;
         std::vector<int> remainingAsteroids;
-        for (int asteroid : asteroids) {
+        bool keepChecking = false;
 
-            // if collisions is empty, then push the asteroid
-            if (collisions.empty()) {
-                collisions.push(asteroid);
-                continue;
-            }
-            
-            int topAsteroid = collisions.top();
+        for (int incomingAsteroid : asteroids) {
 
-            
-
-            // collision is expected
-            bool bothAsteroidsDestroyed = false;
-            while (!collisions.empty()) {
-                topAsteroid = collisions.top();
-
-                // both asteroids travel in the same direction
-                if ((topAsteroid > 0 && asteroid > 0) || (topAsteroid < 0 && asteroid < 0)) { 
-                    collisions.push(asteroid);
-                    break;
+            do {
+                // if stack is empty, then push the asteroid
+                if (collisions.empty()) {
+                    collisions.push(incomingAsteroid);
+                    keepChecking = false;
+                    continue;
                 }
 
-                if (abs(topAsteroid) > abs(asteroid)) break; // incoming asteroid is destroyed
-                if (abs(topAsteroid) < abs(asteroid)) collisions.pop(); // top asteroid is destroyed
-                if (abs(topAsteroid) == abs(asteroid)) { // both asteroids are destroyed
+                // if stack is not empty, check the collision result
+                int topAsteroid = collisions.top();
+
+                // case 1: both asteroids traveling in the same direction
+                if ((topAsteroid > 0 && incomingAsteroid > 0) || (topAsteroid < 0 && incomingAsteroid < 0))
+                    collisions.push(incomingAsteroid);
+
+                // case 2: top asteroid traveling left, incoming asteroid traveling right (no collision)
+                else if (topAsteroid < 0 && incomingAsteroid > 0)
+                    collisions.push(incomingAsteroid);
+
+                // case 3: collision, both asteroids get destroyed
+                else if (topAsteroid == -incomingAsteroid)
                     collisions.pop();
-                    bothAsteroidsDestroyed = true;
-                    break;
-                } 
-            }
 
-            // incoming asteroid destroyed all other asteroids, push it to the vector
-            if (collisions.empty() && !bothAsteroidsDestroyed) collisions.push(asteroid);
+                // case 4: collision, top asteroid gets destroyed by incoming asteroid
+                else if (abs(topAsteroid) < abs(incomingAsteroid)) {
+                    collisions.pop();
+                    keepChecking = true;
+                    continue;
+                }
 
-            // convert stack into vector
-            while (!collisions.empty()) {
-                remainingAsteroids.push_back(collisions.top());
-                collisions.pop();
-            }
+                // case 5: collision, incoming asteroid gets destroyed by top asteroid, stop checking 
+                keepChecking = false;
 
-            // reverse the vector, using "two pointers" algorithm
-            int left = 0;
-            int right = remainingAsteroids.size() - 1;
-            while (left < right) {
-                int temp = remainingAsteroids[left];
-                remainingAsteroids[left] = remainingAsteroids[right];
-                remainingAsteroids[right] = temp;
-                left++;
-                right--;
-            }
+            } while (keepChecking);
+            
         }
+
+        // convert stack into vector
+        while (!collisions.empty()) {
+            remainingAsteroids.push_back(collisions.top());
+            collisions.pop();
+        }
+
+        // reverse the vector, using "two pointers" algorithm
+        int left = 0;
+        int right = remainingAsteroids.size() - 1;
+        while (left < right) {
+            int temp = remainingAsteroids[left];
+            remainingAsteroids[left] = remainingAsteroids[right];
+            remainingAsteroids[right] = temp;
+            left++;
+            right--;
+        }   
+
         return remainingAsteroids;
     }
 };
 
 int main() {
-    std::vector<int> input = {10,2,-5};
+    std::vector<int> input = {-2,-2,1,-2};
     std::cout << "input: [";
     for (int num : input) std::cout << num << " ";
     std::cout << "]" << std::endl;
