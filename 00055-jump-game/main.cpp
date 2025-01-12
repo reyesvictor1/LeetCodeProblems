e19dc72dd6910ea8bd6dcea91dccdeee5170074b
Victor Reyes <100883441+reyesvictor1@users.noreply.github.com>
2025-01-12 15:56:20 -0600

    Problem #55: C++ solution added.

--- 00055-jump-game/main.cpp
+++ 00055-jump-game/main.cpp
@@ -5,27 +5,26 @@ class Solution {
 public:
     bool canJump(std::vector<int>& nums) {
         
-        // if there is only one element, the last index is reached
-        if (nums.size() == 1) return true;
+        // dynamic programming (tabulation)
 
-        // the idea is to iterate in reverse though the vector from the second-to-last index
-        // wherever a zero is found, the jump distance needed increases by one
-        // the next time a non zero number is found:
-        //      if the number is greater than or equal to the jump distance needed, reset the jump distance needed
-        //      if the number is less than the jump distance needed, increase the jump distance needed by one
-        // in the end, the jump distance needed must be one, otherwise the jump to the last index is impossible
+        // pre-allocate the dp table
+        int tSize = nums.size();
+        std::vector<bool> table(tSize, false);
 
-        int jumpDistanceNeeded = 1;
-        for (int idx = nums.size() - 2; idx >= 0; idx--) {
-            if (nums[idx]) {
-                if (nums[idx] >= jumpDistanceNeeded) jumpDistanceNeeded = 1;
-                else jumpDistanceNeeded++;
+        // seed the first value: always reachable, so, it's always true
+        table[0] = true;
+
+        // fill the table
+        for (int i = 0; i < tSize; i++) {
+            if (!table[i]) break;
+            int maxJump = nums[i];
+            for (int j = 1; j <= maxJump; j++) {
+                if (i + j < tSize) table[i + j] = true;
             }
-            else if (!nums[idx]) jumpDistanceNeeded++;
         }
-        if (jumpDistanceNeeded == 1) return true;
-        else return false;
 
+        // return the last index of table 
+        return table[tSize - 1];
     }
 };
 
