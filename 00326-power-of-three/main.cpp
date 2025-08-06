64b3fd1e1cf6833ac9f475e39ee83fee7d32a8b8
Victor Reyes <100883441+reyesvictor1@users.noreply.github.com>
2025-08-06 00:17:10 -0600

    Problem #326: C++ solution added.

---
+++ 00326-power-of-three/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        //base case n < 1: return false, which propagates through the call stack
+        if (n < 1) return false;
+
+        //base case n == 1: 3^0 = 1, return true, which propagates through the call stack
+        if (n == 1) return true;
+
+        // if not divisible by three, return false, which propagates through the call stack
+        if (n % 3) return false; 
+
+        return isPowerOfThree(n / 3);
+    }
+};
+
+int main() {
+    int input;
+    std::cout << "Enter any integer: ";
+    std::cin >> input;
+
+    Solution solution;
+    bool output = solution.isPowerOfThree(input);
+    std::cout << "Is power of three: " << (output ? "true" : "false") << std::endl;
+
+    return 0;
+}
